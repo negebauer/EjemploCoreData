c6bd5ced813af8734fad574cc5cbd5a9b7f8fde3
nicogeb <Nico@Nicolass-MacBook-Pro.local>
2015-06-18 00:23:50 -0300

    Innit

---
+++ EjemploCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EjemploCoreData
+//
+//  Created by Nicolás Gebauer on 17-06-15.
+//  Copyright (c) 2015 Nicolás Gebauer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
