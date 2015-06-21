9d33625bff766490c1ee7c103d03c45922070d19
nicogeb <Nico@Nicolass-MacBook-Pro.local>
2015-06-21 14:18:23 -0300

    UI Ready

---
+++ EjemploCoreData/TablaContactosDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  TablaContactosDelegate.swift
+//  EjemploCoreData
+//
+//  Created by Nicolás Gebauer on 21-06-15.
+//  Copyright (c) 2015 Nicolás Gebauer. All rights reserved.
+//
+
+import UIKit
+
+class TablaContactosDelegate: NSObject, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let c = tableView.dequeueReusableCellWithIdentifier("IDCeldaContacto") as! CeldaContacto
+        return c
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0 //cambiar
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: false)
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if(editingStyle == .Delete ) {
+            
+        }
+    }
+    
+}
