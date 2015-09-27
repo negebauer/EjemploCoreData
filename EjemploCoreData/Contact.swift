2b92e0e86dc53ac9cf376f950971d3e897410f6b
nicogeb <negebauer@uc.cl>
2015-09-27 17:24:56 -0300

    Contact translated and documented

--- EjemploCoreData/Contact.swift
+++ EjemploCoreData/Contact.swift
@@ -9,39 +9,76 @@
 import Foundation
 import CoreData
 
-/// Clase Contacto para manejar las entidades de tipo Contacto de la base de datos.
-class Contacto: NSManagedObject {
+/** Class that represents and manages `Contact` object that is saved in the database.
+- Parameter firstName: The first name of the `Contact`.
+- Parameter surname: The surname of the `Contact`.
+- Parameter number: The phone number of the `Contact`.
+- Parameter cars: The cars that the `Contact` owns.
 
-    @NSManaged var apellido: String
-    @NSManaged var nombre: String
-    @NSManaged var numero: String
-    @NSManaged var autos: NSSet
+- Author: Nicolás Gebauer.
+- Date: 22-06-15.
+- Copyright: © 2015 Nicolás Gebauer.
+- SeeAlso: [Website](http://nicogeb.github.io/EjemploCoreData/).
+*/
+class Contact: NSManagedObject {
 
-    /// Crea un nuevo Contacto, lo agrega a la base de datos, setea los atributos entregados y retorna dicho Contacto.
-    class func new(moc: NSManagedObjectContext, _nombre:String, _apellido:String, _numero:String) -> Contacto {
-        let newItem = NSEntityDescription.insertNewObjectForEntityForName("Contacto", inManagedObjectContext: moc) as! EjemploCoreData.Contacto
-        newItem.nombre = _nombre
-        newItem.apellido = _apellido
-        newItem.numero = _numero
+    @NSManaged var firstName: String
+    @NSManaged var surname: String
+    @NSManaged var number: String
+    @NSManaged var cars: NSSet
+
+    /** Creates a new `Contact` in the database and returns it.
+    - Parameter firstName: The first name of the `Contact`.
+    - Parameter surname: The surname of the `Contact`.
+    - Parameter number: The phone number of the `Contact`.
+    - Parameter cars: The cars that the `Contact` owns.
+    - Returns: `Contact`
+    
+    - Author: Nicolás Gebauer.
+    - Date: 22-06-15.
+    - Copyright: © 2015 Nicolás Gebauer.
+    - SeeAlso: [Website](http://nicogeb.github.io/EjemploCoreData/).
+    */
+    class func new(moc: NSManagedObjectContext, firstName:String, surname:String, number:String) -> Contact {
+        let newContact = NSEntityDescription.insertNewObjectForEntityForName("Contact",
+            inManagedObjectContext: moc) as! EjemploCoreData.Contact
         
-        return newItem
+        newContact.firstName = firstName
+        newContact.surname = surname
+        newContact.number = number
+        
+        return newContact
     }
     
-    /// Asigna un Auto al Contacto agregandolo a su Set de Autos y seteando el atributo dueno del Auto para que corresponda al Contacto.
-    func darAuto(auto:Auto) {
-        var set = autos as! Set<Auto>
-        set.insert(auto)
-        auto.asignarDueno(self)
-        autos = set
+    /** Assigns a `Car` to this `Contact` owned cars.
+    - Parameter car: The `Car` to be given to this `Contact`.
+    
+    - Author: Nicolás Gebauer.
+    - Date: 22-06-15.
+    - Copyright: © 2015 Nicolás Gebauer.
+    - SeeAlso: [Website](http://nicogeb.github.io/EjemploCoreData/).
+    */
+    func giveCar(car:Car) {
+        var set = cars as! Set<Car>
+        set.insert(car)
+        car.assignOwner(self)
+        cars = set
     }
     
-    /// Quita un Auto al Contacto eliminandolo de su Set de Autos y setenado el atributo dueno del Auto a nil.
-    func quitarAuto(auto:Auto) {
-        var set = autos as! Set<Auto>
-        if set.contains(auto) {
-            set.remove(auto)
-            auto.eliminarDueno()
+    /** Removes a `Car` from this `Contact` owned cars, only if it exists.
+    - Parameter car: The `Car` to be taken from to this `Contact`.
+    
+    - Author: Nicolás Gebauer.
+    - Date: 22-06-15.
+    - Copyright: © 2015 Nicolás Gebauer.
+    - SeeAlso: [Website](http://nicogeb.github.io/EjemploCoreData/).
+    */
+    func takeCar(car:Car) {
+        var set = cars as! Set<Car>
+        if set.contains(car) {
+            set.remove(car)
+            car.deleteOwner()
         }
-        autos = set
+        cars = set
     }
 }
