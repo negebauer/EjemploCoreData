2f7b1a9882933962e110e8f46a1e12e01036bead
nicogeb <negebauer@uc.cl>
2015-09-27 17:38:13 -0300

    ContactCell translated and documented, MainStoryboard updated

---
+++ EjemploCoreData/ContactCell.swift
@@ -0,0 +1,22 @@
+//
+//  CeldaContacto.swift
+//  EjemploCoreData
+//
+//  Created by Nicolás Gebauer on 21-06-15.
+//  Copyright (c) 2015 Nicolás Gebauer. All rights reserved.
+//
+
+import UIKit
+
+/** Cell that shows the information of a `Car`.
+
+- Author: Nicolás Gebauer.
+- Date: 21-06-15.
+- Copyright: © 2015 Nicolás Gebauer.
+- SeeAlso: [Website](http://nicogeb.github.io/EjemploCoreData/).
+*/
+class ContactCell: UITableViewCell {
+    @IBOutlet weak var LabelName: UILabel!
+    @IBOutlet weak var LabelNumber: UILabel!
+    
+}
